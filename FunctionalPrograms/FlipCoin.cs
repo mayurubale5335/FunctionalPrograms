f5ee9d7aa9e4ffc6d22a01f925dc43c9f197bc8c
unknown <mayurubale5335@gmail.com>
2023-04-15 13:00:07 +0530

    [mayur ubale] Modify Flip Coin

---
+++ FunctionalPrograms/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class FlipCoin
+    {
+        const double Validation_Check = 0.5;
+        public void Flip(int n)
+        {
+            int tail = 0;
+            int head = 0;
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                double value = random.NextDouble();
+                if (value < Validation_Check)
+                {
+                    tail++;
+                }
+                else
+                {
+                    head++;
+                }
+            }
+            double Head_Percentage = (head * 100) / n;
+            double Tail_Percentage = (tail * 100) / n;
+            Console.WriteLine("Head Percentage = " + Head_Percentage);
+            Console.WriteLine("Tail Percentage = " + Tail_Percentage);
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -6,13 +6,31 @@ using System.Threading.Tasks;
 
 namespace FunctionalPrograms
 {
-     class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programs");
-            Console.ReadLine();
-
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select an option to execute the program 1.Flip Coin\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the no of flips");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Flip(n);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                        
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
